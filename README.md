0cc680dcc82edf2181a242ccf7a8c9ff8170f0de
FlayaN <hannes.feldt@gmail.com>
2017-05-13 23:42:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# B3Utils
+
+This is a test app with api for testing new technologies with react-native, typescript and dot.net core
+
+## Client
+
+### App
+
+Made using react-native with typescript
+
+#### DevEnv
+```
+npm install -g react-native-cli typescript tsc
+```
+
+#### Start app
+```
+npm install
+npm run start:ios
+```
+OR
+```
+npm run start:android
+```
+
+## Server
+
+Made using dot.net core 2.0
+
+#### DevEnv
+```
+Install VS2017
+Install dot.net core 2.0
+```
+
+#### Start api
+
+1. Select Api as startup
+2. Start (f5)
---
+++ Server/Api/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
+            //services.AddDbContext<DbContext>();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+        }
+    }
+}
